7aa6c69c4f6d1c1e36e9d123859d8698b3e94893
Patrick Sheehan <patrickanthony.sheehan@yahoo.com>
2013-07-14 19:38:45 -0500

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -7,3 +7,14 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+
+
+def sign_in(user)
+  
+  visit signin_path
+  fill_in "Email", :with => user.email.upcase
+  fill_in "Password", :with => user.password
+  click_button "Sign in"
+  #Sign in when not using capybara as well
+  cookies[:remember_token] = user.remember_token
+end
