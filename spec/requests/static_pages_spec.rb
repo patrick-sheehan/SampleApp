6e5707e675f6d6642a6fc8d056b8bda849dd1495
Patrick Sheehan <patrickanthony.sheehan@yahoo.com>
2013-08-04 15:26:22 -0500

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
   
   subject { page }
   describe "Home page" do
@@ -22,6 +22,17 @@ describe "StaticPages" do
         user.feed.each do |item|
           page.should have_selector("li##{item.id}", :text => item.content)
         end
+      end
+      
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+        
+        it { should have_link("0 following", :href => following_user_path(user)) }
+        it { should have_link("1 followers", :href => followers_user_path(user)) }
       end
     end
   end
